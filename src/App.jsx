0c2ca91596cdaa8388db4094131e998a01d71175
rakafdil <rakafadillah123@gmail.com>
2025-07-16 13:52:22 +0700

    add components and basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './navbar.jsx'
+import Navbar from './components/Navbar/navbar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <Navbar />  
+        <Navbar />
       </div>
     </>
   )
