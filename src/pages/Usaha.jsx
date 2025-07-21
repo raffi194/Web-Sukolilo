33918341986678eec9b411af9307c0a21e22b8c9
rakafdil <rakafadillah123@gmail.com>
2025-07-21 08:26:50 +0700

    Add CardUsaha component for displaying business information
    - Implemented CardUsaha component with props for image, title, description, address, link, and contact.
    - Styled the component with Tailwind CSS for responsive design and hover effects.
    - Included FontAwesome icon for visual enhancement.
    - Added a button component for user interaction.

--- src/pages/Usaha.jsx
+++ src/pages/Usaha.jsx
@@ -1,8 +1,44 @@
 import Hero from "../components/Hero";
 import Section from "../components/Section";
 import Button from "../components/Button";
+import CardUsaha from "../components/Usaha/CardUsaha.jsx";
 
 const Usaha = () => {
+
+    const categories = [
+        {
+            id: 1,
+            category: "Elektronik"
+        },
+        {
+            id: 2,
+            category: "Fashion"
+        },
+        {
+            id: 3,
+            category: "Kuliner"
+        },
+        {
+            id: 4,
+            category: "Kerajinan"
+        },
+        {
+            id: 5,
+            category: "Properti"
+        },
+        {
+            id: 6,
+            category: "Warung/Kios"
+        },
+        {
+            id: 7,
+            category: "Bahan Makanan"
+        },
+        {
+            id: 8,
+            category: "Lainnya"
+        },
+    ];
 
 
     return (
@@ -12,7 +48,69 @@ const Usaha = () => {
                 description="Selamat datang di bagian Pembuatan Surat website Desa Krisik! Kami hadir untuk memudahkan Anda dalam setiap langkah pembuatan surat yang Anda butuhkan. Di sini, kami menyediakan berbagai layanan pembuatan surat yang dirancang untuk memenuhi kebutuhan administrasi warga desa.">
 
                 <div className="flex flex-row justify-center items-center pt-5 gap-x-5">
-                    <Button text="Elektronik" onClick={() => alert('Diklik!')} />
+                    {categories.map((category) => (
+                        <Button key={category.id} text={category.category} onClick={() => alert(`Kategori ${category.category} diklik!`)} />
+                    ))}
+                </div>
+
+                <div className="py-15 flex flex-wrap justify-center items-start gap-15 mt-10">
+
+                    <CardUsaha
+                        image="src/assets/img/bengkel.png"
+                        title="Angkasa Motorcycle Tube"
+                        description="Melayani Tambal Ban, perbaikan mesin motor, isi gas roda motor."
+                        address="JL. Pegangsaan Timur No.65 Jakarta"
+                        linkTo="#"
+                    />
+                    <CardUsaha
+                        image="src/assets/img/bengkel.png"
+                        title="Angkasa Motorcycle Tube"
+                        description="Melayani Tambal Ban, perbaikan mesin motor, isi gas roda motor."
+                        address="JL. Pegangsaan Timur No.65 Jakarta"
+                        linkTo="#"
+                    />
+                    <CardUsaha
+                        image="src/assets/img/bengkel.png"
+                        title="Angkasa Motorcycle Tube"
+                        description="Melayani Tambal Ban, perbaikan mesin motor, isi gas roda motor."
+                        address="JL. Pegangsaan Timur No.65 Jakarta"
+                        linkTo="#"
+                    />
+                    <CardUsaha
+                        image="src/assets/img/bengkel.png"
+                        title="Angkasa Motorcycle Tube"
+                        description="Melayani Tambal Ban, perbaikan mesin motor, isi gas roda motor."
+                        address="JL. Pegangsaan Timur No.65 Jakarta"
+                        linkTo="#"
+                    />
+                    <CardUsaha
+                        image="src/assets/img/bengkel.png"
+                        title="Angkasa Motorcycle Tube"
+                        description="Melayani Tambal Ban, perbaikan mesin motor, isi gas roda motor."
+                        address="JL. Pegangsaan Timur No.65 Jakarta"
+                        linkTo="#"
+                    />
+                    <CardUsaha
+                        image="src/assets/img/bengkel.png"
+                        title="Angkasa Motorcycle Tube"
+                        description="Melayani Tambal Ban, perbaikan mesin motor, isi gas roda motor."
+                        address="JL. Pegangsaan Timur No.65 Jakarta"
+                        linkTo="#"
+                    />
+                    <CardUsaha
+                        image="src/assets/img/bengkel.png"
+                        title="Angkasa Motorcycle Tube"
+                        description="Melayani Tambal Ban, perbaikan mesin motor, isi gas roda motor."
+                        address="JL. Pegangsaan Timur No.65 Jakarta"
+                        linkTo="#"
+                    />
+                    <CardUsaha
+                        image="src/assets/img/bengkel.png"
+                        title="Angkasa Motorcycle Tube"
+                        description="Melayani Tambal Ban, perbaikan mesin motor, isi gas roda motor."
+                        address="JL. Pegangsaan Timur No.65 Jakarta"
+                        linkTo="#"
+                    />
                 </div>
 
             </Section>
