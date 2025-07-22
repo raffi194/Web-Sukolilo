c8b17991160f388e445945ec1245ab98fce9bdf8
rakafdil <rakafadillah123@gmail.com>
2025-07-22 13:44:20 +0700

    fixed navbar

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,34 +9,44 @@ import Navigation from './Navigation';
 const Footer = () => {
   return (
     <footer
-      className=" text-white py-10 flex gap-10"
+      className="text-white py-10"
       style={{ backgroundColor: "var(--clr-primary-5)" }}
     >
-      <div className="container mx-auto">
-        <div className="flex gap-9 w-full">
-          <div className='flex-start'>
+      <div className="container mx-auto px-4">
+        <div className="flex gap-12 w-full">
+          <div className='flex-none'>
             <img src={logo} alt="Logo" className="h-16 mb-4" />
           </div>
-          <div className="flex items-start justify-between flex-1">
-            <div className='flex-start flex-col grow'>
-              <p style={{ fontSize: "var(--size-medium-small)" }} className="font-semibold pb-4">Pemerintah Desa Sukolilo</p>
-              <p style={{ fontSize: "var(--size-small)" }} className="font-normal">
+          <div className="flex justify-between flex-1 gap-16">
+            <div className='flex flex-col'>
+              <p className="font-semibold pb-4" style={{ fontSize: "var(--size-medium-small)" }}>
+                Pemerintah Desa Sukolilo
+              </p>
+              <p className="font-normal max-w-xs" style={{ fontSize: "var(--size-small)" }}>
                 Jalan Raya Sukolilo RT.03 RW 01 <br />
                 Desa Sukolilo, Kecamatan Jabung,<br />
                 Kabupaten Malang<br />
                 Provinsi Jawa Timur, 65163
               </p>
             </div>
-            <div className='flex-start flex-col grow'>
-              <p style={{ fontSize: "var(--size-medium-small)" }} className="font-semibold pb-4">Hubungi Kami</p>
-              <p style={{ fontSize: "var(--size-small)" }} className="font-normal">
-                <FontAwesomeIcon icon={faPhone} style={{ paddingRight: "6px" }} />
-                082212345678 <br />
-                <FontAwesomeIcon icon={faEnvelope} style={{ paddingRight: "6px", paddingTop: "12px" }} />
-                sukolilo@gmail.com
+
+            <div className='flex flex-col'>
+              <p className="font-semibold pb-4" style={{ fontSize: "var(--size-medium-small)" }}>
+                Hubungi Kami
+              </p>
+              <p className="font-normal space-y-3" style={{ fontSize: "var(--size-small)" }}>
+                <span className="flex items-center gap-2">
+                  <FontAwesomeIcon icon={faPhone} />
+                  082212345678
+                </span>
+                <span className="flex items-center gap-2 pt-3">
+                  <FontAwesomeIcon icon={faEnvelope} />
+                  sukolilo@gmail.com
+                </span>
               </p>
             </div>
-            <div className='flex-start flex-col grow'>
+
+            <div className='flex flex-col'>
               <p style={{ fontSize: "var(--size-medium-small)" }} className="font-semibold pb-4">Jelajahi</p>
               <p style={{ fontSize: "var(--size-small)" }} className="font-normal">
                 <a
@@ -69,13 +79,14 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="text-center self-stretch pt-18">
-          <p style={{ fontSize: "var(--size-small)" }} className="font-medium">2025 Powered by KKN Tim 33 Fakultas Ilmu Komputer Desa Sukolilo 2025</p>
+        <div className="text-center pt-16">
+          <p className="font-medium" style={{ fontSize: "var(--size-small)" }}>
+            2025 Powered by KKN Tim 33 Fakultas Ilmu Komputer Desa Sukolilo 2025
+          </p>
         </div>
       </div>
     </footer>
   );
 }
-
 
 export default Footer;
